a72c15c60e725c80d4198ff504b2452a06a402e7
HP <hannah.paulson@itech.media>
2022-03-18 05:38:04 -0600

    added buttton component & button group component

---
+++ src/components/ButtonGroup.vue
@@ -0,0 +1,24 @@
+<template>
+        <Button v-for="button in buttons"
+        :key="button.id"
+        :label="button.label"/>
+</template>
+
+<script>
+import Button from'./Button.vue'
+import { reactive, computed } from 'vue'
+
+export default {
+    name: 'ButtonGroup',
+    components: { Button },
+    props: {
+        buttons: { type: Array, required: true, default: () => [] },
+    },
+      setup(props) {
+    props = reactive(props);
+    return {
+      isEmpty: computed(() => props.buttons.length === 0),
+    };
+  },
+};
+</script>
